f3d1707a199e263c21eee379622bde9200171a39
McFarlin <34318060+EthanMcFarlin@users.noreply.github.com>
2022-07-18 22:29:28 -0400

    Create a map tutorial

---
+++ js/main.js
@@ -0,0 +1,20 @@
+require([
+    "esri/config",
+    "esri/Map",
+    "esri/views/MapView"
+], function (esriConfig,Map, MapView) {
+
+    esriConfig.apiKey = "AAPKdfcc4a7dcc7e4176a137c38b87516e64P2haNjbiyKdPRWUk4feYG-Es6ebTY3T8bYJRXrSN-2gUKssSU5kF4T6brCoTDrEq";
+
+    const map = new Map({
+        basemap: "arcgis-topographic" // Basemap layer service
+    });
+
+    const view = new MapView({
+        map: map,
+        center: [-118.805, 34.027], // Longitude, latitude
+        zoom: 13, // Zoom level
+        container: "viewDiv" // Div element
+    });
+
+});
