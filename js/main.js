8366de747f4135d7a0545538acd6434738a96532
McFarlin <34318060+EthanMcFarlin@users.noreply.github.com>
2022-07-25 19:02:14 -0400

    Implemented functionality to swap between variables

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
+let defaultColorField = "pct_black";
+let defaultLegend = "% population identifying as black (color) ";
+let defaultLowerStop = 0.0;
+let defaultUpperStop = 1.0;
+let defaultLowerLabel = "0%";
+let defaultUpperLabel = "100%";
+
+
 require([
     "esri/config",
     "esri/Map",
     "esri/views/SceneView",
     "esri/layers/FeatureLayer",
-    "esri/widgets/Legend"
-
-], function(esriConfig, Map, SceneView, FeatureLayer, Legend) {
+    "esri/widgets/Legend",
+    "esri/widgets/Expand"
+], function(esriConfig, Map, SceneView, FeatureLayer, Legend, Expand) {
 
     esriConfig.apiKey = "AAPKdfcc4a7dcc7e4176a137c38b87516e64P2haNjbiyKdPRWUk4feYG-Es6ebTY3T8bYJRXrSN-2gUKssSU5kF4T6brCoTDrEq";
 
@@ -41,20 +49,20 @@ require([
             },
             {
                 type: "color",
-                field: "pct_black",
+                field: defaultColorField,
                 legendOptions: {
-                    title: "% population identifying as black (color) "
+                    title: defaultLegend
                 },
                 stops: [
                     {
-                        value: 0.0,
+                        value: defaultLowerStop,
                         color: "#d1e1ff",
-                        label: "0%"
+                        label: defaultLowerLabel
                     },
                     {
-                        value: 1.0,
+                        value: defaultUpperStop,
                         color: "#144db8",
-                        label: "100%"
+                        label: defaultUpperLabel
                     }
                 ]
             }
@@ -115,10 +123,123 @@ require([
         }
     });
 
+
     const legend = new Legend({
         view: view
     });
 
     view.ui.add(legend, "bottom-right");
 
+    const selectionMenu = document.getElementById("variable-selector");
+    contentInsidePopup = new Expand({
+        expandIconClass: "esri-icon-sliders-horizontal",
+        expanded: true,
+        view: view,
+        content: selectionMenu
+    });
+    view.ui.add(contentInsidePopup, {
+        position: "top-left",
+        index: 1
+    });
+
+    const demographicHolder = document.getElementById("demographic-holder");
+    demographicHolder.addEventListener('change', function (e) {
+        let changedField = e.target;
+
+        switch (changedField.id) {
+            case 'population':
+                setDemographicVariable("population", "population", 0.0, 500000, "0 people", "500,000 people")
+                break;
+            case 'pct_female':
+                console.log('pct_female 2');
+                setDemographicVariable("pct_female", "% population identifying as female (color)", 0.4, 0.7, "40%", "70%")
+                break;
+            case 'pct_age_under_5':
+                setDemographicVariable("pct_age_un", "% population under the age of 5 (color)", 0.00, 0.2, "0%", "20%")
+                break;
+            case 'pct_age_over_85':
+                setDemographicVariable("pct_age_ov", "% population over the age of 85 (color)", 0.0, 0.2, "0%", "20%")
+                break;
+            case 'pct_white':
+                setDemographicVariable("pct_white", "% population identifying as white (color)", 0.0, 1.0, "0%", "100%")
+                break;
+            case 'pct_black':
+                setDemographicVariable("pct_black", "% population identifying as black (color)", 0.0, 1.0, "0%", "100%")
+                break;
+            case 'pct_native':
+                setDemographicVariable("pct_native", "% population identifying as native (color)", 0.0, 1.0, "0%", "100%")
+                break;
+            case 'pct_asian':
+                setDemographicVariable("pct_asian", "% population identifying as asian (color)", 0.0, 0.4, "0%", "40%")
+                break;
+            case 'pct_two_or_more_races':
+                setDemographicVariable("pct_two_or", "% population identifying as two or more races (color)", 0.0, 1.0, "0%", "100%")
+                break;
+            case 'pct_hispanic':
+                setDemographicVariable("pct_hispan", "% population identifying as hispanic (color)", 0.0, 1.0, "0%", "100%")
+                break;
+            case 'n_households':
+                setDemographicVariable("n_househol", "number of households (color)", 0.0, 100000.0, "0 households", "100,000 households")
+                break;
+            case 'pct_households_single_father':
+                setDemographicVariable("pct_househ", "% households with a single father (color)", 0.0, 1.0, "0%", "100%")
+                break;
+            case 'pct_households_single_mother':
+                setDemographicVariable("pct_hous_1", "% households with a single mother (color)", 0.0, 1.0, "0%", "100%")
+                break;
+            case 'n_housing_units':
+                setDemographicVariable("n_housing_", "number of housing units (color)", 0.0, 100000.0, "0 housing units", "100,000 housing unnits")
+                break;
+            case 'n_occupied_housing_units':
+                setDemographicVariable("n_occupied", "number of occupied housing units (color)", 0.0, 100000.0, "0 housing units", "100,000 housing units")
+                break;
+            case 'pct_renting':
+                setDemographicVariable("pct_rentin", "% renting (color)", 0.0, 1.0, "0%", "100%")
+                break;
+        }
+
+    });
+
+    function setDemographicVariable
+    (field, legend, lower_stop, upper_stop, lower_label, upper_label) {
+        console.log("check: setDemographicVariable");
+        defaultColorField = field;
+        defaultLegend = legend;
+        defaultLowerStop = lower_stop;
+        defaultUpperStop = upper_stop;
+        defaultLowerLabel = lower_label;
+        defaultUpperLabel = upper_label;
+
+        refreshRenderer(field, legend, lower_stop, upper_stop, lower_label, upper_label);
+    }
+
+    function refreshRenderer(field, legend, lower_stop, upper_stop, lower_label, upper_label) {
+        console.log("check: refresh rendererer");
+        new_renderer = customLayer.renderer.clone();
+        const colorVariable = new_renderer.visualVariables[1];
+        colorVariable.field = field;
+        colorVariable. legendOptions =
+            {
+                title: legend
+            }
+        colorVariable.stops =
+         [
+            {
+                value: lower_stop,
+                color: "#d1e1ff",
+                label: lower_label
+            },
+            {
+                value: upper_stop,
+                color: "#144db8",
+                label: upper_label
+            }
+        ]
+        customLayer.renderer = new_renderer;
+    }
+
+
+
 });
+
+
